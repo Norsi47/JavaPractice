55331ba837805ef946812f86f3ac6e8f486256f6
Norsi47 <briggsnorsi@yahoo.com>
2022-03-18 22:32:39 -0400

    more to dos

--- OnMyOwnProj/src/ShoppingButtons/ShoppingButtonsService.java
+++ OnMyOwnProj/src/ShoppingButtons/ShoppingButtonsService.java
@@ -8,3 +8,6 @@ public class ShoppingButtonsService {
 //can use "fix me", no space to remmind to fix
 //TODO:Create Buttons with pictures of food items
 //TODO: Make sure all buttons function properly
+/*
+* TODO:save selected files into a txt file
+*  show seleced items in files on console or on jpanel output*/
