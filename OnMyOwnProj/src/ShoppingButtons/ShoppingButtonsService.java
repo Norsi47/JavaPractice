9d75245166c5739f98a71259c845837cadfa590f
Norsi47 <briggsnorsi@yahoo.com>
2022-03-20 13:49:21 -0400

    starting out shop button message box

--- OnMyOwnProj/src/ShoppingButtons/ShoppingButtonsService.java
+++ OnMyOwnProj/src/ShoppingButtons/ShoppingButtonsService.java
@@ -1,6 +1,45 @@
 package ShoppingButtons;
 
+import javax.swing.*;
+
 public class ShoppingButtonsService {
+    //outside frame
+    JFrame frame;
+    //inside
+    JPanel panel;
+
+    /*Array for dif buttons
+    need to set function for reach button*/
+
+    JButton[] functionButtons = new JButton[9];
+
+    //for calculations
+    double num1 = 0, num2 = 0, result = 0;
+
+    //these are for the symbols
+    char symbolsOperator;
+
+    public ShoppingButtonsService() {
+        //the name of box
+        frame = new JFrame("ShopButtonBox");
+        //to close box when x is pressed
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000, 500);
+        frame.setLayout(null);
+
+
+
+
+
+
+        //has to be at the bottom
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        ShoppingButtonsService shoppingButtonsService = new ShoppingButtonsService();
+    }
 
 
 }
