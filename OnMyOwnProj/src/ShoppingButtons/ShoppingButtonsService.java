a8610f973b8af6bce4d2fbd357ebe9df729e1bc7
Norsi47 <briggsnorsi@yahoo.com>
2022-03-22 16:24:10 -0400

    trying to add pictures into buttons

--- OnMyOwnProj/src/ShoppingButtons/ShoppingButtonsService.java
+++ OnMyOwnProj/src/ShoppingButtons/ShoppingButtonsService.java
@@ -1,6 +1,9 @@
 package ShoppingButtons;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShoppingButtonsService {
     //outside frame
@@ -8,16 +11,26 @@ public class ShoppingButtonsService {
     //inside
     JPanel panel;
 
+    //for displayBox
+    JTextField displayBox;
+
     /*Array for dif buttons
     need to set function for reach button*/
 
     JButton[] functionButtons = new JButton[9];
+
+    //for fruit button pics
+    JButton[] fruitButtonPics = new JButton[3];
+    JButton fruitButtons;
 
     //for calculations
     double num1 = 0, num2 = 0, result = 0;
 
     //these are for the symbols
     char symbolsOperator;
+
+    //for font
+    Font myFont = new Font("TimesRoman", Font.BOLD, 40);
 
     public ShoppingButtonsService() {
         //the name of box
@@ -27,11 +40,41 @@ public class ShoppingButtonsService {
         frame.setSize(1000, 500);
         frame.setLayout(null);
 
+        displayBox = new JTextField();
+        //for size and display
+        displayBox.setBounds(200, 50, 400, 60);
+        displayBox.setFont(myFont);
+        displayBox.setEditable(false);
+
+        panel = new JPanel();
+        panel.setBounds(50, 100, 300, 300);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+        panel.setBackground(Color.CYAN);
+
+        //adding images to button
+        List<JButton> buttonsList = new ArrayList<JButton>();
+        String[] paths = {"apple.png", "banana.jpg", "Tomato.jpg" };
+        for(int i = 0; i < 3; i++) {
+            ImageIcon imageIcon = new ImageIcon(paths[i]);
+
+            fruitButtons = new JButton(imageIcon);
+            buttonsList.add(fruitButtons);
+            panel.add(fruitButtons);
+
+
+
+        }
 
 
 
 
 
+
+
+
+
+        frame.add(panel);
+        frame.add(displayBox);
         //has to be at the bottom
         frame.setVisible(true);
 
