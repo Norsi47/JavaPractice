3eb8352c1eab017f4ec43bce358ec8a19193d285
Norsi47 <briggsnorsi@yahoo.com>
2022-04-01 20:52:03 -0400

    added numbers 1-0(10) to app

--- OnMyOwnProj/src/ShoppingButtons/ShoppingButtonsService.java
+++ OnMyOwnProj/src/ShoppingButtons/ShoppingButtonsService.java
@@ -119,6 +119,13 @@ public class ShoppingButtonsService implements ActionListener {
         panel.add(numberButtons[1]);
         panel.add(numberButtons[2]);
         panel.add(numberButtons[3]);
+        panel.add(numberButtons[4]);
+        panel.add(numberButtons[5]);
+        panel.add(numberButtons[6]);
+        panel.add(numberButtons[7]);
+        panel.add(numberButtons[8]);
+        panel.add(numberButtons[9]);
+        panel.add(numberButtons[0]);
 
 
 
